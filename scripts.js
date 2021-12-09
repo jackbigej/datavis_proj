9803750817feb287154d2a1f50abf07cda9e18af
Jack Strong <jstrong5@nd.edu>
2021-12-09 11:44:41 -0500

    user input

--- scripts.js
+++ scripts.js
@@ -1,10 +1,101 @@
-d3.json("test_data.json", parseData);
+// d3.json("test_data.json", parseData);
+d3.json("all-years-data.json", parseData);
+
+// global variables to store data
+var playerNames = [];
+var allData = {};
 
 function parseData(error, data) {
-    console.log(data);
+    allData = data;
+    for (const [key, value] of Object.entries(data)) {
+        playerNames.push(key)
+    }
+    console.log(allData);
+    console.log(playerNames);
+
+    // create datalist entries
+    var d1 = document.createElement('datalist');
+    var d2 = document.createElement('datalist');
+    var d3 = document.createElement('datalist');
+    var d4 = document.createElement('datalist');
+    var d5 = document.createElement('datalist');
+    var d6 = document.createElement('datalist');
+
+    for (let i = 0; i < playerNames.length; i++) {
+        var option1 = document.createElement('option');
+        var option2 = document.createElement('option');
+        var option3 = document.createElement('option');
+        var option4 = document.createElement('option');
+        var option5 = document.createElement('option');
+        var option6 = document.createElement('option');
+        option1.value = playerNames[i];
+        option2.value = playerNames[i];
+        option3.value = playerNames[i];
+        option4.value = playerNames[i];
+        option5.value = playerNames[i];
+        option6.value = playerNames[i];
+        d1.appendChild(option1);
+        d2.appendChild(option2);
+        d3.appendChild(option3);
+        d4.appendChild(option4);
+        d5.appendChild(option5);
+        d6.appendChild(option6);
+    }
+    // append to first input
+    d1.id = 'datalistOne1';
+    document.getElementById('firstPlayer1').appendChild(d1);
+
+    // append to second input
+    d2.id = 'datalistTwo1';
+    document.getElementById('secondPlayer1').appendChild(d2);
+    d3.id = 'datalistTwo2';
+    document.getElementById('secondPlayer2').appendChild(d3);
+
+    // append to third input
+    d4.id = 'datalistThree1';
+    document.getElementById('thirdPlayer1').appendChild(d4);
+    d5.id = 'datalistThree2';
+    document.getElementById('thirdPlayer2').appendChild(d5);
+    d6.id = 'datalistThree3';
+    document.getElementById('thirdPlayer3').appendChild(d6);
 }
 
-function drawSinglePlayer() {
+function displayVisual() {
+    // read radio button
+    var radioBtn = document.getElementsByName('numPlayers');
+
+    for (let i = 0; i < radioBtn.length; i++) {
+        if (radioBtn[i].checked) {
+            radioBtnValue = radioBtn[i].value;
+        }
+    }
+
+    // read player options
+    let playerOption1 = '';
+    let playerOption2 = '';
+    let playerOption3 = '';
+    switch(radioBtnValue) {
+        case 'one':
+            playerOption1 = document.getElementById('firstPlayer1').value;
+            drawSinglePlayer(playerOption1);
+            break;
+        case 'two':
+            playerOption1 = document.getElementById('secondPlayer1').value;
+            playerOption2 = document.getElementById('secondPlayer2').value;
+            drawTwoPlayers(playerOption1, playerOption2);
+            break;
+        case 'three':
+            playerOption1 = document.getElementById('thirdPlayer1').value;
+            playerOption2 = document.getElementById('thirdPlayer2').value;
+            playerOption3 = document.getElementById('thirdPlayer3').value;
+            drawThreePlayers(playerOption1, playerOption2, playerOption3);
+            break;
+    }
+}
+
+function drawSinglePlayer(player1) {
+    console.log(player1);
+    
     var svg = d3.select("svg");
     svg.selectAll('path').remove();
 
@@ -38,7 +129,10 @@ function drawSinglePlayer() {
 
 }
 
-function drawTwoPlayers() {
+function drawTwoPlayers(player1, player2) {
+    console.log(player1);
+    console.log(player2);
+
     var svg = d3.select("svg");
     svg.selectAll('path').remove();
 
@@ -75,7 +169,11 @@ function drawTwoPlayers() {
         .attr('stroke', 'white');
 }
 
-function drawThreePlayers() {
+function drawThreePlayers(player1, player2, player3) {
+    console.log(player1);
+    console.log(player2);
+    console.log(player3);
+
     var svg = d3.select("svg");
     svg.selectAll('path').remove();
 
